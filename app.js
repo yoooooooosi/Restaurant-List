8a74538817f54c6c23f85e81efb7a3750a37b508
LAPTOP-8420NR94\amber <amber898952@gmail.com>
2023-08-20 14:30:33 +0800

    feat: make the program more concise

--- app.js
+++ app.js
@@ -60,27 +60,8 @@ app.get("/restaurants/new", (req, res) => {
 });
 
 app.post("/restaurants",(req,res) =>{
-  const name = req.body.name; 
-  const name_en = req.body.name_en; 
-  const category = req.body.category; 
-  const image = req.body.image; 
-  const location = req.body.location; 
-  const phone = req.body.phone;   
-  const google_map = req.body.google_map; 
-  const rating = req.body.rating; 
-  const description = req.body.description; 
 
-  return Restaurant.create({
-    name,
-    name_en,
-    category,
-    image,
-    location,
-    phone,
-    google_map,
-    rating,
-    description,
-  })
+  return Restaurant.create(req.body)
     .then(() => res.redirect("/"))
     .catch((error) => console.log(error));
 
@@ -96,29 +77,12 @@ app.get("/restaurants/:restaurant_id/edit", (req, res) => {
 });
 
 app.post("/restaurants/:restaurant_id/edit", (req,res)=>{
-  const id = req.params.restaurant_id
-  const name = req.body.name; 
-  const name_en = req.body.name_en; 
-  const category = req.body.category; 
-  const image = req.body.image; 
-  const location = req.body.location; 
-  const phone = req.body.phone;   
-  const google_map = req.body.google_map; 
-  const rating = req.body.rating; 
-  const description = req.body.description; 
+  //使用OObject.assign
+  const id = req.params.restaurant_id;
 
   return Restaurant.findById(id)
     .then((restaurant) => {
-      restaurant.name = name;
-      restaurant.name_en = name_en;
-      restaurant.category = category;
-      restaurant.image = image;
-      restaurant.location = location;
-      restaurant.phone = phone;
-      restaurant.google_map = google_map;
-      restaurant.rating = rating;
-      restaurant.description = description;
-
+      Object.assign(restaurant, req.body);
       return restaurant.save();
     })
     .then(() => res.redirect(`/restaurants/${id}`))
