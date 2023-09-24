15b74366d5ab63d503e26410f61855477c922d2b
LAPTOP-8420NR94\amber <amber898952@gmail.com>
2023-09-24 23:54:57 +0800

    feat: add express-session

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 //設定基本套件
 const express = require("express");
+const session = require("express-session");
 const exphbs = require("express-handlebars");
 const bodyParser = require('body-parser')// 引用 body-parser
 const methodOverride = require('method-override')
@@ -14,6 +15,15 @@ const port = 3000;
 //設定樣板引擎，其中全局layoutj為main.handlebars
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars"); //
+
+app.use(
+  session({
+    secret: "ThisIsMySecret",
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+
 
 //告知express靜態檔案路徑
 app.use(express.static("public")); //告知express靜態檔案放置在public資料夾中
