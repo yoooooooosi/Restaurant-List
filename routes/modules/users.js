95cd0317e331d80831a740786706dff7425967c6
LAPTOP-8420NR94\amber <amber898952@gmail.com>
2023-09-24 23:04:29 +0800

    feat: add register and login page

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -8,4 +8,13 @@ router.get("/login", (req, res) => {
   res.render("login");
 });
 
+router.post("/login", (req, res) => {});
+
+
+
+//註冊表單頁面 ,GET /users/register
+router.get("/register", (req, res) => {
+  res.render("register");
+});
+
 module.exports = router; //匯出路由模組
