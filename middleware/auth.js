cdfd2f45eb173bff6ade220a0801ac8d03637b64
LAPTOP-8420NR94\amber <amber898952@gmail.com>
2023-09-25 16:41:54 +0800

    feat: add user authentication in todo and home routers

---
+++ middleware/auth.js
@@ -0,0 +1,10 @@
+//此檔會匯出一個物件，而物件中有authenticator 的函式
+module.exports = {
+  authenticator: (req, res, next) => {
+    //req.isAuthenticated()會根據 request 的登入狀態回傳 true 或 false
+    if (req.isAuthenticated()) {
+      return next(); //登入成功(true)進入下一個 middleware
+    }
+    res.redirect("/users/login"); //登入失敗(false)，強制返回 login 頁面
+  },
+};
--- routes/index.js
+++ routes/index.js
@@ -6,10 +6,13 @@ const home = require('./modules/home') // 引入 home 模組程式碼
 const restaurant = require('./modules/restaurant') // 引入 restaurant 模組程式碼
 const users = require("./modules/users"); 
 
+const { authenticator } = require("../middleware/auth");  // 掛載 middleware
+//從middleware/auth中拿取名為authenticator的函數
+
 //如果符合路徑，便將request導向各自模組
-router.use('/', home) 
-router.use("/restaurants", restaurant);
+router.use("/restaurants", authenticator, restaurant);
 router.use("/users", users);
+router.use("/", authenticator, home); 
 
 //匯出路由器
 module.exports = router;
