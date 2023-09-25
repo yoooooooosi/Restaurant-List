65224764137e52119c0ed51f958e6f2d5182faa1
LAPTOP-8420NR94\amber <amber898952@gmail.com>
2023-09-25 22:39:21 +0800

    modify todoSeeder

--- models/seeds/restaurantSeeder.js
+++ models/seeds/restaurantSeeder.js
@@ -1,14 +1,65 @@
+const bcrypt = require("bcryptjs");
 
-const Restaurant = require('../restaurant') //載入model
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+
+const User = require("../user");
+const Restaurant = require("../restaurant"); //載入model
 const restaurantList = require("../../restaurant.json").results; //匯入restaurant的seeder(它包在results內)
 const db = require("../../config/mongoose");
 
-//連線成功
-db.once('open',()=>{
-  Restaurant.create(restaurantList)
-    .then(()=> {
-      console.log("seeder done");
-      db.close(); //將資料庫關閉，避免佔記憶體
+const SEED_USER01 = {
+  name: "user1",
+  email: "user1@example.com",
+  password: "12345678",
+};
+
+const SEED_USER02 = {
+  name: "user2",
+  email: "user2@example.com",
+  password: "12345678",
+};
+
+const createUserAndRestaurants = (user, restaurantIds) => {
+  //先建立包含user和restaurantIds參數的函數
+
+  return (
+    bcrypt //開始使用雜湊函數
+      .genSalt(10)
+      .then((salt) => bcrypt.hash(user.password, salt))
+      .then((hash) =>
+        //建立user
+        User.create({
+          name: user.name,
+          email: user.email,
+          password: hash,
+        })
+      )
+      //餐廳部分
+      .then((user) => {
+        //當user建立完畢後，執行下一步
+        const promises = restaurantIds.map((id) => {
+          //利用map將restaurantIds中的id一一執行出來
+          const restaurant = restaurantList.find((data) => data.id === id); //根據 id 找到相應的餐廳
+          restaurant.userId = user._id; // 為找到的餐廳設置 userId
+          return Restaurant.create(restaurant); // 創建餐廳並返回 Promise
+        });
+        return Promise.all(promises); //確保每個值跑完
+      })
+      .then(() => {
+        console.log(`${user.name} and restaurants seeded successfully.`);
+      })
+  );
+};
+
+db.once("open", () => {
+  createUserAndRestaurants(SEED_USER01, [1, 2, 3]) 
+  //傳入 SEED_USER01 和 [1, 2, 3] 作為參數，並建立其user及restaurantIds
+    .then(() => createUserAndRestaurants(SEED_USER02, [4, 5, 6])) //再來執行第二位user
+    .then(() => {
+      console.log("All seed data added successfully!");
+      process.exit();
     })
-    .catch((error) => console.log(error));
-})//用來監聽是否有觸發open
+    .catch((error) => console.error(error));
+});
